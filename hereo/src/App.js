b0b8a07bc7e630cc72a87711dd702565598eac51
ParkSangKyu <108622613+sangkyu39@users.noreply.github.com>
2024-09-24 11:07:02 +0000

    기본 메인 페이지 세팅

--- hereo/src/App.js
+++ hereo/src/App.js
@@ -1,5 +1,19 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+//로딩할 모든 페이지들을 routes에서 가져오기
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Main from "./routes/Main.js";
+
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div>
+			<Router>
+				<Routes>
+					<Route path="/" element={<Main />} />
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
---
+++ hereo/src/routes/Main.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React, { uesEffect, useState } from "react";
+function Main() {
+  return (
+  <div>
+  </div>
+)
+}
+
+export default Main;
